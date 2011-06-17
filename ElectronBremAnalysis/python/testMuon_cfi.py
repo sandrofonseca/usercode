cd3b648c0145bb7c7fd16b163e43adf7a836b1af
sandrofonseca <>
2011-06-17 13:15:28 +0000

    This commit was manufactured by cvs2svn to create tag 'V00-00-10'.
    Sprout from master 2011-06-17 13:15:27 UTC sfonseca 'Change tag'
    Delete:
        ElectronBremAnalysis/BuildFile
        ElectronBremAnalysis/interface/ElectronBremAnalysis.h
        ElectronBremAnalysis/python/__init__.py
        ElectronBremAnalysis/python/muonbrem_cfi.py
        ElectronBremAnalysis/python/muonbremanalysis_cfi.py
        ElectronBremAnalysis/python/testMuon_cfi.py
        ElectronBremAnalysis/src/ElectronBremAnalysis.cc
        ElectronBremAnalysis/src/module.cc
        ElectronBremAnalysis/test/crab.cfg
        ElectronBremAnalysis/test/simpleplots_photons.C
        ElectronBremAnalysis/test/simpleplots_photons.C~
        ElectronBremAnalysis/test/simpleplots_photons2.C
        ElectronBremAnalysis/test/testElectron_cfg.py
        FastMuonAnalyzer/BuildFile.xml
        FastMuonAnalyzer/interface/TrackerMuonAnalyzer.h
        FastMuonAnalyzer/src/TrackerMuonAnalyzer.cc
        FastMuonAnalyzer/test/SingleMuPt1000_cfi_py_GEN_FASTSIM_HLT_VALIDATION.py
        FastMuonAnalyzer/test/SingleMuPt1000_cfi_py_GEN_FASTSIM_HLT_VALIDATION.pyc
        FastMuonAnalyzer/test/TrackerMuonAnalyzer_FASTfromFILE.py
        FastMuonAnalyzer/test/TrackerMuonAnalyzer_FULL_noHLT.py
        FastMuonAnalyzer/test/crab.cfg
        FastMuonAnalyzer/test/crab.history
        FastMuonAnalyzer/test/defineCrab.csh
        FastMuonAnalyzer/test/macroPlots.C
        FastMuonAnalyzer/test/macroPlots2.C
        FastMuonAnalyzer/test/macroPlotspt2.C
        FastMuonAnalyzer/test/ptPlots.C
        FastMuonAnalyzer/test/run.sh
        FastMuonAnalyzer/test/runHLT.sh
        FastMuonAnalyzer/test/submit.csh
        FastMuonAnalyzer/test/submitHLT.csh
        Photon_FastSim_Analyzer/test/BuildFile.xml
        Photon_FastSim_Analyzer/test/testEvent.cc
        Photon_FastSim_Analyzer/test/testEvent_cfg.py
        timing_cpu_test/timing.cpp

--- ElectronBremAnalysis/python/testMuon_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-XMLIdealGeometryESSource = cms.ESSource("XMLIdealGeometryESSource",
-    geomXMLFiles = cms.vstring('Geometry/CMSCommonData/data/materials.xml', 
-        'SimG4CMS/MuonBremAnalysis/data/testMuon.xml'),
-    rootNodeName = cms.string('testMuon:TestMuon')
-)
-
-
